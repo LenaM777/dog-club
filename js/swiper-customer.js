31d565b9e5921c92dc5689a071ed909095c78a51
Olena Mustafina <toysamiy.mail@gmail.com>
2025-03-14 16:01:11 +0200

    Create slider-customer

---
+++ js/swiper-customer.js
@@ -0,0 +1,22 @@
+new Swiper('.slider-customer', {
+    // Optional parameters
+    
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.slider-button.next',
+      prevEl: '.slider-button.prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+  

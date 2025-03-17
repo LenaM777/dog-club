cbeb2964997b2b6804f8be8bc8e7f5114613a1ec
Olena Mustafina <toysamiy.mail@gmail.com>
2025-03-17 15:28:33 +0200

    Update category section: create card update slider

--- js/swiper-category.js
+++ js/swiper-category.js
@@ -1,7 +1,7 @@
 new Swiper('.slider-category', {
     loop: true,
     slidesPerView: 1, 
-    spaceBetween: 20,
+    spaceBetween: 24,
     breakpoints: {
     768: { slidesPerView: 3 },
     1280: { slidesPerView: 4 }
